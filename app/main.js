7ae11a565e419e13e2903be937ee0e43296df109
Shubham Kumar <hello@shubhkumar.in>
2021-10-04 12:53:46 +0530

    Added code to bind to 6379

--- app/main.js
+++ app/main.js
@@ -3,8 +3,8 @@ const net = require("net");
 console.log("Your code goes here!");
 
 // Uncomment this block to pass the first stage
-// const server = net.createServer(socket => {
-//   socket.pipe(socket);
-// });
-//
-// server.listen(6379, '127.0.0.1');
+const server = net.createServer(socket => {
+  socket.pipe(socket);
+});
+
+server.listen(6379, '127.0.0.1');
