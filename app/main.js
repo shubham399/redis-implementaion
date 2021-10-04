6f5b4e2221fb70b1b08c78e31a5b27cf1c5516e4
Shubham Kumar <hello@shubhkumar.in>
2021-10-04 13:59:53 +0530

    Added echoHandler.

--- app/main.js
+++ app/main.js
@@ -1,23 +1,31 @@
 const net = require("net");
 
 const getSimpleString = str => "+" + str + "\r\n"
+const getBulkStringReply = str => str ? `\$${str.length}\r\n${str}\r\n` : "$-1\r\n"
 const getErrorString = str => "-" + str + "\r\n"
 const getIntegerResponse = str => ":" + str + "\r\n"
 
 const pingHandler = (socket) => {
   socket.write(getSimpleString("PONG"))
 }
+const echoHandler = (socket, data) => {
+  console.log(data.join(" "))
+  socket.write(getBulkStringReply(data.join(" ")))
+}
 
 
 const server = net.createServer((socket) => {
   const dataHandler = (buffer) => {
     // Do it in stream instead of Buffering it.
-    const data = buffer.toString('utf-8').toUpperCase().trim().split(/\s/).filter(a => a !== "").filter(a => a[0] !== "$");
-    const command = data[1]
-    console.log('Request from', socket.remoteAddress, 'port', socket.remotePort, data);
+    const data = buffer.toString('utf-8').trim().split(/\s/).filter(a => a !== "").filter(a => a[0] !== "$");
+    const command = data[1].toUpperCase()
+    console.log('Request from', socket.remoteAddress, 'port', socket.remotePort, command);
     switch (command) {
       case 'PING':
         pingHandler(socket)
+        break;
+      case 'ECHO':
+        echoHandler(socket, data.slice(2))
         break;
       default:
         socket.write("\n")
