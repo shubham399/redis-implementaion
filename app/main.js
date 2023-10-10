91abcddc0b7b22aaa21300ca42a48d00a8a23407
Shubham Kumar <hello@shubhkumar.in>
2023-10-10 10:14:57 +0530

    Added config get handler

--- app/main.js
+++ app/main.js
@@ -1,8 +1,27 @@
+'use strict';
+
 const net = require("net");
-let mem = {}
+const mem = {};
+const options = {};
+
+// Iterate through the command-line arguments and create a map
+for (let i = 2; i < process.argv.length; i++) {
+  const arg = process.argv[i];
+  // Check if the argument is in the format "--key=value"
+  if (arg.startsWith('--')) {
+    const key = arg.slice(2).toLowerCase();
+    const value = process.argv[i + 1]
+    options[key] = value || true; // If there's no value, set it to true
+  }
+}
 
 const getSimpleString = str => "+" + str + "\r\n"
 const getBulkStringReply = str => str ? `\$${str.length}\r\n${str}\r\n` : "$-1\r\n"
+const getArrayReply = (arr) => {
+  const processed = arr.map(getBulkStringReply);
+  return `*${processed.length}\r\n${processed.join("")}`
+
+}
 const getErrorString = str => "-" + str + "\r\n"
 const getIntegerResponse = str => ":" + str + "\r\n"
 
@@ -28,23 +47,46 @@ const getHandler = (socket, data) => {
   socket.write(getBulkStringReply(mem[data[0]]))
 }
 
+const getConfigHandler = (socket, data) => {
+  console.log("🚀 ~ file: main.js:52 ~ getConfigHandler ~ data:", data)
+  const command = data[0].toUpperCase();
+  const option = data[1].toLowerCase();
+  console.log('Config Handler', command);
+  switch (command) {
+    case 'GET':
+      const val = options[option]
+      socket.write(getArrayReply([option, val]))
+  }
+}
+const keysHandler = (socket, data) => {
+  const keys = Object.keys(mem);
+  socket.write(getBulkStringReply("LOL"))
+}
+
+
 const server = net.createServer((socket) => {
   const dataHandler = (buffer) => {
     const data = buffer.toString('utf-8').trim().split(/\s/).filter(a => a !== "").filter(a => a[0] !== "$");
-    const command = data[1].toUpperCase()
+    const command = data[0].toUpperCase()
     console.log('Request from', socket.remoteAddress, 'port', socket.remotePort, command);
     switch (command) {
       case 'PING':
         pingHandler(socket)
         break;
       case 'ECHO':
-        echoHandler(socket, data.slice(2))
+        echoHandler(socket, data.slice(1))
         break;
       case 'SET':
-        setHandler(socket, data.slice(2))
+        setHandler(socket, data.slice(1))
+        break;
+      case 'KEYS':
+        keysHandler(socket, data.slice(1))
         break;
       case 'GET':
-        getHandler(socket, data.slice(2))
+        getHandler(socket, data.slice(1))
+        break;
+      case 'CONFIG':
+        getConfigHandler(socket, data.slice(1))
         break;
       default:
         socket.write("\n")
